f4fec4a9a417f5f6d9283402903a8e0ffbef9a5c
Caleb Herrera <caleo010@gmail.com>
2025-05-06 23:23:07 -0400

    Skeleton laid down - menu ui and page setup

--- MohonkMountainHouse/MainPage.xaml.cs
+++ MohonkMountainHouse/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -17,14 +18,39 @@ using Windows.UI.Xaml.Navigation;
 
 namespace MohonkMountainHouse
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+    /// <summary>  
+    /// An empty page that can be used on its own or navigated to within a Frame.  
+    /// </summary>  
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void golfPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(GolfPage));
+        }
+
+        private void trailsPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(TrailsPage));
+        }
+
+        private void directionsPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(DirectionsPage));
+        }
+
+        private void websitePageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(WebsitePage));
+        }
+
+        private void menuPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MenuPage));
         }
     }
 }
